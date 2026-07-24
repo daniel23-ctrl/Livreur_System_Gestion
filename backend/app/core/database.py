bd36116b1714214a108b03ebc37afe7e03359042
daniel23-ctrl <dannyakoumany@gmail.com>
2026-07-23 23:38:43 -0700

    feat: creation de endpoint livreur creation

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,9 +1,19 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
-
 from app.core.config import settings
+import ssl
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=True,
+    connect_args={"ssl": ssl_context},
+    pool_pre_ping=True,
+    pool_recycle=300,
+)
 
 AsyncSessionLocal = sessionmaker(
     engine,
